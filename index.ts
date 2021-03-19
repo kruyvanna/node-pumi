f385382be30ddbde57919a1b77011a7d6163ffa4
Kruy Vanna <kruyvanna@gmail.com>
2021-03-19 19:07:32 +0700

    expose api

--- index.ts
+++ index.ts
@@ -36,16 +36,52 @@ app.get('/districts/:provinceId', async (req, res) => {
   res.send(districts)
 })
 
+app.get('/district/nameKm/:name', async (req, res) => {
+  const { name } = req.params
+  const district = districtApi.findByNameKm(name)
+  res.send(district)
+})
+
+app.get('/district/nameLatin/:name', async (req, res) => {
+  const { name } = req.params
+  const district = districtApi.findByNameLatin(name)
+  res.send(district)
+})
+
 app.get('/communes/:districtId', async (req, res) => {
   const { districtId } = req.params
   const communes = communeApi.getByDistrictId(districtId)
   res.send(communes)
 })
 
+app.get('/commune/nameKm/:name', async (req, res) => {
+  const { name } = req.params
+  const commune = communeApi.findByNameKm(name)
+  res.send(commune)
+})
+
+app.get('/commune/nameLatin/:name', async (req, res) => {
+  const { name } = req.params
+  const commune = communeApi.findByNameLatin(name)
+  res.send(commune)
+})
+
 app.get('/villages/:communeId', async (req, res) => {
   const { communeId } = req.params
   const villages = villageApi.getByCommuneId(communeId)
   res.send(villages)
+})
+
+app.get('/village/nameKm/:name', async (req, res) => {
+  const { name } = req.params
+  const village = villageApi.findByNameKm(name)
+  res.send(village)
+})
+
+app.get('/village/nameLatin/:name', async (req, res) => {
+  const { name } = req.params
+  const village = villageApi.findByNameLatin(name)
+  res.send(village)
 })
 
 const port = process.env.PORT || 3002
